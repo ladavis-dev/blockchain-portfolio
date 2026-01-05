8056be37fe822bbe4b2a35ac24438c01c353bc57
ladavis-dev <l.a.davis@nxtcybersolns.com>
2026-01-04 22:50:03 -0500

    Fix submodule names to match projects folder paths

--- projects/foundry-testing-portfolio/week-01-storage/script/Storage.s.sol
+++ projects/foundry-testing-portfolio/week-01-storage/script/Storage.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "forge-std/console2.sol";
-import "../src/Storage.sol";
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {Storage} from "../src/Storage.sol";
 
 /// @title StorageScript
 /// @notice Deployment script for Storage.sol
